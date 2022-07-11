a3cadb7524a4480217f3ee1e8bed2cf0f85f6d62
Megha8150 <meghananayak436@gmail.com>
2022-07-11 14:18:07 +0530

    Welcome message

---
+++ LineComparisonAssignment/LineComparison.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonAssignment
+{
+   public class LineComparison
+    {
+        public static void lineComparison()
+        {
+        Console.WriteLine("Welcome to line comparison computation program");
+        }
+    }
+}
